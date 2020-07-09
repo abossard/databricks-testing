8f500a309637dd45ffbc890cb363d77c6e7ba612
Andre Bossard <Andre.Bossard@microsoft.com>
2020-07-09 14:39:59 +0000

    changes from the demo

--- src/do_work.py
+++ src/do_work.py
@@ -6,19 +6,20 @@ from pyspark import SparkContext
 
 def do_work(item):
   print("Work for Job: " + str(item))
-  delay = random.randint(1,20)
+  delay = random.randint(1,2)
   time.sleep(delay)
   result = dict(**item)
   result.update(delay=delay)
   return result
 
+def execute_spark_load(sc, input):
+  jobinput = input
+  ji_df = sc.parallelize(jobinput, 10)
+  return ji_df.map(do_work).collect()
+
 if __name__ == "__main__":
     spark = SparkSession.builder.getOrCreate()
     sc = spark.sparkContext
+    print(execute_spark_load(sc, [dict(name="job_" + str(y))  for y in range(10)]))
     
-    #sc = SparkContext(master='local[2]', appName=__name__)
     
-    jobinput = [dict(name="job_" + str(y))  for y in range(10)]
-    ji_df = sc.parallelize(jobinput, 10)
-    result = ji_df.map(do_work).collect()
-    print(result)
--- src/do_work_test.py
+++ src/do_work_test.py
@@ -1,4 +1,7 @@
-from do_work import do_work
+from do_work import do_work, execute_spark_load
+from pyspark import SparkContext
+
+sc = SparkContext(master="local[2]")
 
 def test_do_work():
     test_input = {}
@@ -6,3 +9,13 @@ def test_do_work():
     result = do_work(test_input)
 
     assert result['delay']
+
+def test_execute_spark():
+    result = execute_spark_load(sc, [dict()])
+
+    assert len(result) > 0
+
+def test_execute_spark2():
+    result = execute_spark_load(sc, [dict()])
+
+    assert len(result) > 0
